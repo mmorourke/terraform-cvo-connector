8a6d884d7329e6a55d550a0473a7d91cc3136077
mmorourke <orourke@netapp.com>
2021-11-16 15:46:25 -0600

    Update connector.tf

--- connector.tf
+++ connector.tf
@@ -11,7 +11,7 @@ resource "netapp-cloudmanager_connector_aws" "cl-occm-aws" {
   instance_type               = "t3.xlarge"
   subnet_id                   = var.subnetid
   security_group_id           = var.security_group
-  iam_instance_profile_name   = "orourke-fsx-connectorOCCM1630334910744-OCCMInstanceProfile-1BCX43RF7A0CW"
+  iam_instance_profile_name   = "orourke-fsx-connectorOCCM1630334910744-OCCMInstancexxxxxxxx-1BCX43RF7A0CW"
   account_id                  = var.account
   associate_public_ip_address = true
 }
